1061382a326a0379fb8c78ece39f293dcef37fac
Shirley Azurin <wheeloftheyear159@gmail.com>
2023-01-04 23:46:45 -0500

    Actividad Asincrona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 's8-asincrona';
+
+  nombre!: string;
+  apellido!: string;
+  edad!: number;
+
+  message!: string;
+  bienvenido!: string;
+
+  datos!: [string, string, any];
+
+  mostrar: boolean = false
+
+  Verificar(edad:any) {
+    if(edad > 18) {
+      this.message = 'Mayor de edad';
+      this.bienvenido = `Bienvenido ${this.nombre} ${this.apellido}`;
+      this.datos = [this.nombre, this.apellido, this.edad];
+
+      this.mostrar = true;
+    } else {
+      this.message = 'Menor de edad'
+      this.bienvenido = '';
+      this.datos = ['','',''];
+    }
+  }
 }
